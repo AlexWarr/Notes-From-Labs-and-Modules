7d4d7c381cef4af21422f01e6cdef2b411811b0d
unknown <lxwarr@gmail.com>
2022-09-04 18:03:29 -0600

    First Commit of Module Notes and Lab code

---
+++ Week 1 Application 1 Module 1.java
@@ -0,0 +1,36 @@
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * primitive:
+		 * int - integer
+		 * double - decimal places
+		 * float - more precise
+		 * long - int with high number range
+		 * short - int with low range
+		 * byte - 8 bits of data
+		 * char - single charachter
+		 * boolean - true/fals
+		 * 
+		 * Objects:
+		 * String - textual data
+		 */
+		
+		variable declaration - type, identifier, assignment operator, value, semicolon
+		int numberOfFriends;
+		int samsAge = 67;
+		double accountBalance = 34.67;
+		char middleInitial = 'C';
+		boolean IsHotOutside = true;
+		String firstName = "Sam";
+		
+		System.out.println(samsAge);
+		System.out.println(accountBalance);
+		System.out.println(middleInitial);
+		System.out.println(IsHotOutside);
+		System.out.println(firstName);
+
+	}
+
+}
---
+++ Week 1 Lab 1 Part 1.java
@@ -0,0 +1,27 @@
+
+public class VariablesAndOperations {
+
+	public static void main(String[] args) {
+		//create a variable to hold the quantity of available plane seats left on a flight
+		int availableSeats = 12;
+		//create a variable to hold the cost of groceries at checkout
+		double groceryBill = 59.99;
+		//create a variable to hold a person's middle initial
+		char middleInitial = 'c';
+		//create a variable to hold true if it's hot outside and false if it's cold outside
+		boolean isHotOutside = true;
+		//create a variable to hold a customer's first name
+		String customerFirstName = "Jerry";
+		//create a variable to hold a street address
+		String address = "666 West Ira Street";
+		//print all variables to the console
+		System.out.println("Seats available: " + availableSeats);
+		System.out.println("Grocery Bill: " + groceryBill);
+		System.out.println("Middle Initial: " + middleInitial);
+		System.out.println("It is hot outside. " + isHotOutside);
+		System.out.println("Customer First Name: " + customerFirstName);
+		System.out.println("Street Address: " + address);
+
+	}
+
+}
